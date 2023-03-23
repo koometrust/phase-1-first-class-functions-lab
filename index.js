43f69b65410bafa3503ebef91a2be3585a983818
Koome <judekoome360@gmail.com>
2023-03-23 17:06:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+function returnFirstTwoDrivers(array) {
+    return array.slice(0, 2);
+  }
+  
+function returnLastTwoDrivers (array) {
+    return array.slice( -2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+function createFareMultiplier(multiplier){
+    return fuction
+}
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+
+  function fareDoubler(a){
+    return a*2
+  }
+
+  fareTripler = (a)=> a*3
+
+  function selectDifferentDrivers(drivers, driverselector){
+    return driverselector(drivers);
+  }
+
+  function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+  }
+
+  function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+  }
